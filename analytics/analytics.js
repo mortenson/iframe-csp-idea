5cee40ee4f85d592bc0ae1c1ccbed96cb119f5e7
Sam Mortenson <mortensonsam@gmail.com>
2025-02-12 12:14:27 -0800

    Further assert CSP metatag behavior.

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -1,3 +1,6 @@
+// Try to mess with the CSP to allow us to connect to backend.
+document.getElementById('csp-meta-tag').setAttribute('content', "default-src 'self'; script-src 'self' http://localhost:9338; connect-src 'self' http://localhost:9338;")
+
 window.addEventListener('load', () => {
   fetch('http://localhost:9338/hello').then(res => res.text()).then(text => {
     document.getElementById("output").innerText += `\nGot "${text}" from analytics backend.`
@@ -5,7 +8,7 @@ window.addEventListener('load', () => {
     document.getElementById("output").innerText += `\nFailed to contact analtyics backend.`
   })
 
-  fetch('http://localhost:9337/hello').then(res => res.text()).then(text => {
+  fetch('/hello').then(res => res.text()).then(text => {
     document.getElementById("output").innerText += `\nGot "${text}" from our backend. This should not happen!`
   }).catch(() => {
     document.getElementById("output").innerText += `\nFailed to contact our backend, which is expected.`
